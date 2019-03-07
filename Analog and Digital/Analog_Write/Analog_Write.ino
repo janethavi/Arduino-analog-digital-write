06226f8ce62d0ffe5354b9fe0ff0a9f2358863f8
Janeth Fernando <janethavishka@gmail.com>
2019-03-08 00:36:52 +0530

    Add files via upload

---
+++ Analog and Digital/Analog_Write/Analog_Write.ino
@@ -0,0 +1,21 @@
+
+/* Code to control the brightness of the LED(Analog signal)*/ 
+
+int LED1 = 13;
+
+void setup() {
+   pinMode(LED1, OUTPUT);
+}
+
+
+void loop() {
+  /* Wave strength number is from 0- 255. You can put a number from 0 to 255. */
+  
+  analogWrite(LED1, 50);  // turn on LED1 and wave strength is 50
+  delay(300);                  // wait for 300ms
+  analogWrite(LED1, 120);  // turn on LED1 and wave strength is 120
+  delay(300);                  // wait for 300ms       
+  analogWrite(LED1, 225);  // turn on LED1 and wave strength is 225
+  delay(300);                  // wait for 300ms
+
+}
---
+++ Analog and Digital/Digital_write/Digital_write.ino
@@ -0,0 +1,14 @@
+/* Code to blink a LED (digital wave)*/
+int LED1 = 13; // initialize the port 
+
+void setup() {
+   pinMode(LED1, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(LED1, HIGH);   // turn on LED1 
+  delay(200);                  // wait for 200ms
+  digitalWrite(LED1, LOW);    // turn off LED1
+  delay(200);                  // wait for 200ms       
+ }
